73479f314b2ed8a921a5a72b511a05d7b0f6cc97
narayana <mandavalakshminarayana446@gmail.com>
2025-08-09 17:00:58 +0530

    updated project

--- src/main/java/com/klef/ClientController.java
+++ src/main/java/com/klef/ClientController.java
@@ -13,4 +13,8 @@ public class ClientController {
 	public String demo(){
 		return "Jenkins demo";
 	}
+	@GetMapping("/test")
+	public String test(){
+		return "Deploy Spring Boot using Jenkins";
+	}
 }
